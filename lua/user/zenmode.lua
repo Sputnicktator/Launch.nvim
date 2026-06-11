d505608c48b590adf4fc5ea83d4c598251cf0c9a
sputnicktator <njc86@cornell.edu>
2026-06-11 00:48:32 -0400

    Fix zenmode

--- lua/user/zenmode.lua
+++ lua/user/zenmode.lua
@@ -3,7 +3,21 @@ return {
   cmd = 'ZenMode',
   opts = {
     window = {
-      width = 0.85,
+      width = 1,
     },
+    on_open = function()
+      vim.opt.number = false
+      vim.opt.relativenumber = false
+      vim.opt.signcolumn = 'no'
+      vim.opt.colorcolumn = ''
+      vim.g.indent_blankline_enabled = false
+    end,
+    on_close = function()
+      vim.opt.number = true
+      vim.opt.relativenumber = false
+      vim.opt.signcolumn = 'yes'
+      vim.opt.colorcolumn = ''
+      vim.g.indent_blankline_enabled = true
+    end,
   },
 }
